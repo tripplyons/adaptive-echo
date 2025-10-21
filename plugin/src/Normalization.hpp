0423e1a72bdd47a448a7a9b21b5f1fdd4e66bb78
Tripp Lyons <gogs@localhost>
2025-10-21 17:43:24 -0400

    add normalization and improve training

---
+++ plugin/src/Normalization.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+using namespace std;
+
+vector<int32_t> normalize(const vector<double> &x) {
+    double headroom = 0.95;
+    double maxValue = double(INT32_MAX) * headroom;
+    double scale = maxValue * headroom;
+    double largestAbs = 0;
+    for (double x : x) {
+        largestAbs = max(largestAbs, abs(x));
+    }
+    vector<int32_t> normalized(x.size());
+    for (int i = 0; i < x.size(); i++) {
+        normalized[i] = int32_t(x[i] / largestAbs * scale);
+    }
+    return normalized;
+}
