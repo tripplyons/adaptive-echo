7b1e3052cbea5765786fdf68d32a58d38a6d2a82
BoundlessFate <villac3@rpi.edu>
2025-10-28 16:52:30 -0400

    wav handler hpp and cpp tested separately. verified to work to write to and read from wav files extremely quickly and accurately...

--- plugin/src/WavHandler.hpp
+++ plugin/src/WavHandler.hpp
@@ -1,9 +1,10 @@
-#include <vector>
-#include <string>
-#include <utility>
 #include <cstdint>
 #include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
 // Writes the data and sample rate to the file
-void writeData(const std::string& filename, const std::vector<int32_t>& data, int sample_rate);
+void writeData(const std::string &filename, const std::vector<int32_t> &data,
+               int sample_rate);
 // Returns the data and sample rate as a pair
-std::pair<std::vector<int32_t>*,int> loadData(const std::string& filename);
+std::pair<std::vector<int32_t> *, int> loadData(const std::string &filename);
