566ed34806bec867a95f8eaa965125fd39d71b85
BoundlessFate <villac3@rpi.edu>
2025-10-31 16:59:19 -0400

    working on finishing up dataset generator

--- dataset-generator/src/Main.cpp
+++ dataset-generator/src/Main.cpp
@@ -4,8 +4,10 @@
 #include <fstream>
 #include <random>
 #include <thread>
-void runProgram(int numFiles, char *outputPath, int nWeights, int nThreads);
-double** initializeCsv(char* outputPath, int numFiles, int nWeights);
+#include "../../plugin/src/WavHandler.hpp"
+#include "../../plugin/src/Oscillator.hpp"
+void runProgram(int numFiles, char *outputPath, int nThreads);
+double** initializeCsv(char* outputPath, int numFiles);
 void generateAllFiles(char *outputPath, double **weightArr, int weightArrLen, int rowLen, int nThreads);
 void generateFilesForThread(char *outputPath, double **weightArr, int weightArrLen, int rowLen, int threadNum, int nThreads);
 void generateFile(char *outputPath, double* weights, int rowLen);
@@ -14,8 +16,8 @@ void generateFile(char *outputPath, double* weights, int rowLen);
 // Many checks to see if args are valid, and then calls rest of code
 int main(int argc, char** argv) {
     try {
-        if (argc != 5) {
-            std::cout << "Usage: adaptive-echo <num-files> <local-path-to-output> <nweights> <nthreads>" << std::endl;
+        if (argc != 4) {
+            std::cout << "Usage: adaptive-echo <num-files> <local-path-to-output> <nthreads>" << std::endl;
             return -1;
         }
         try {
@@ -23,11 +25,7 @@ int main(int argc, char** argv) {
             if (numFiles <= 0) {
                 throw std::invalid_argument("Argument less than or equal to 0");
             }
-            int nWeights = std::stoi(argv[3]);
-            if (nWeights <= 0) {
-                throw std::invalid_argument("Argument less than or equal to 0");
-            }
-            int nThreads = std::stoi(argv[4]);
+            int nThreads = std::stoi(argv[3]);
             if (nThreads <= 0) {
                 throw std::invalid_argument("Argument less than or equal to 0");
             }
@@ -35,7 +33,7 @@ int main(int argc, char** argv) {
             std::cout << "Usage: argument 1,3,4 must be a positive integer" << std::endl;
             return -1;
         }
-        runProgram(std::stoi(argv[1]), argv[2], std::stoi(argv[3]), std::stoi(argv[4]));
+        runProgram(std::stoi(argv[1]), argv[2], std::stoi(argv[4]));
         return 0;
     // A general catch all to avoid ugly popup on uncaught error. Blanket catch all
     } catch (const std::exception &e) {
@@ -44,9 +42,9 @@ int main(int argc, char** argv) {
     }
 }
 // Creates csv then creates files
-void runProgram(int numFiles, char *outputPath, int nWeights, int nThreads) {
-    double** weightArr = initializeCsv(outputPath, numFiles, nWeights);
-    generateAllFiles(outputPath, weightArr, numFiles, nWeights, nThreads);
+void runProgram(int numFiles, char *outputPath, int nThreads) {
+    double** weightArr = initializeCsv(outputPath, numFiles);
+    generateAllFiles(outputPath, weightArr, numFiles, nThreads);
     for (int i = 0; i < numFiles; i++) {
         delete[] weightArr[i];
     }
@@ -55,7 +53,7 @@ void runProgram(int numFiles, char *outputPath, int nWeights, int nThreads) {
 
 // Creates a 2d array of doubles
 // Each row is is the list of values associated with
-double** initializeCsv(char* outputPath, int numFiles, int nWeights) {
+double** initializeCsv(char* outputPath, int numFiles) {
 
     // Sets up random number generator
     std::random_device rd;
@@ -64,8 +62,8 @@ double** initializeCsv(char* outputPath, int numFiles, int nWeights) {
 
     double** weightArr = new double *[numFiles];
     for (int i = 0; i < numFiles; i++) {
-        weightArr[i] = new double[nWeights];
-        for (int j = 0; j < nWeights; j++) {
+        weightArr[i] = new double[8];
+        for (int j = 0; j < 8; j++) {
             // Random weights across the matrix
             weightArr[i][j] = dist(gen);
         }
@@ -73,7 +71,7 @@ double** initializeCsv(char* outputPath, int numFiles, int nWeights) {
     std::ofstream ofs(outputPath);
     for (int i = 0; i < numFiles; i++) {
         ofs << "file" << (i+1) << ".wav";
-        for (int j = 0; j < nWeights; j++) {
+        for (int j = 0; j < 8; j++) {
             ofs << "," << weightArr[i][j];
         }
         ofs << "\n";
@@ -84,7 +82,19 @@ double** initializeCsv(char* outputPath, int numFiles, int nWeights) {
 // ------------------ FILE GENERATION BELOW --------------------------
 // Create individual file with given settings
 void generateFile(char *outputPath, double* weights, int rowLen) {
-
+    double maxTime = 5;
+    int sampleRate = 44100;
+    double timeInterval = 1.0 / sampleRate;
+    double nSamples = maxTime * sampleRate;
+    vector<uint32_t>* data = new double[(int)nSamples];
+    // Add samples in order by time
+    for (int i=0; i<nSamples; i++) {
+        (*data).push_back(osc_uniform(std::mt19937(), i * timeInterval, weights[0],
+            weights[1], weights[2], weights[3], weights[4], weights[5], weights[6], weights[7]));
+    }
+    // Write data to file and free memory
+    writeData(outputPath, data, sampleRate);
+    free(data);
 }
 // Executes file generation for files assigned to a specific thread
 void generateFilesForThread(char* outputPath, double** weightArr, int weightArrLen, int rowLen, int threadNum, int nThreads) {
