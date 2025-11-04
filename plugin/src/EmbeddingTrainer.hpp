b52237cba176868ccb7143faced693d9b8b9084b
HenryTheall <thealh@rpi.edu>
2025-11-04 17:09:45 -0500

    train_step function
    created a train_step function that will update the parameters of the sounds and settings embedding matrix to decrease loss for training step

---
+++ plugin/src/EmbeddingTrainer.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <autodiff/reverse/var.hpp>
+#include <autodiff/reverse/var/grad.hpp>
+#include <cmath>
+#include <stdexcept>
+#include <vector>
+#include <random>
+#include <functional>
+#include <iostream>
+
+#include "LossGradient.cpp"
+
+class EmbeddingTrainer {
+public:
+    EmbeddingTrainer(int num_pairs, int dim, double lr){
+        N = num_pairs;
+        embedding_dim = dim;
+        learning_rate = lr;
+
+        // construct sounds and settings matrices with random values
+
+    }
+
+    void train(int num_epochs, const autodiff::var& tau){
+        // train sounds and settings matrices
+    }
+
+
+
+
+private:
+    int N;
+    int embedding_dim;
+    double learning_rate;
+
+    std::vector<std::vector<autodiff::var>> sounds;
+    std::vector<std::vector<autodiff::var>> settings;
+
+    double train_step(const autodiff::var& tau){
+        // compute loss with forward pass from lossgradient.cpp
+        autodiff::var loss = forward(tau, sounds, settings);
+
+        // do a backward pass to get a gradient for each variable
+        // all_params will store the gradient for each variable of the sounds AND settings matrices
+        std::vector<std::reference_wrapper<autodiff::var>> all_params;
+        all_params.reserve(N * embedding_dim * 2);
+
+        // fill all_params with current sounds and settings values
+        for (auto& row : sounds) {
+            for (auto& var : row) {
+                all_params.push_back(var);
+            }
+        }
+        for (auto& row : settings) {
+            for (auto& var : row) {
+                all_params.push_back(var);
+            }
+        }
+
+        // gradients stores the gradient for each variable in all_params
+        auto gradients = autodiff::grad(loss, all_params);
+
+        // descend the gradient and update parameters
+        for(size_t i = 0; i < all_params.size(); i++){
+            // take step in opposite direction of loss gradient to decrease loss
+            all_params[i].get() -= learning_rate * gradients[i];
+        }
+
+        return autodiff::val(loss);
+    }
+
+};
