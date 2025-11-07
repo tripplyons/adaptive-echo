2ec2e694ef03629071989af7245338c7ff3351f6
Tripp Lyons <tripplyons@gmail.com>
2025-11-07 17:09:55 -0500

    setup importable pytorch graph for c++

--- adaptive_echo_python/src/adaptive_echo_python/encoder.py
+++ adaptive_echo_python/src/adaptive_echo_python/encoder.py
@@ -17,15 +17,13 @@ class EncoderLayer(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, input_size, embedding_size, hidden_size, num_layers):
         super(Encoder, self).__init__()
-        self.embedding = nn.Embedding(input_size, embedding_size)
-        self.project_in = nn.Linear(embedding_size, hidden_size)
+        self.project_in = nn.Linear(input_size, hidden_size)
         self.project_out = nn.Linear(hidden_size, embedding_size)
         self.layers = nn.ModuleList(
             [EncoderLayer(hidden_size, hidden_size) for _ in range(num_layers)]
         )
 
     def forward(self, x):
-        x = self.embedding(x)
         x = self.project_in(x)
         for layer in self.layers:
             x = layer(x)
--- adaptive_echo_python/src/adaptive_echo_python/export_graphs.py
+++ adaptive_echo_python/src/adaptive_echo_python/export_graphs.py
@@ -1,16 +1,18 @@
 from pathlib import Path
-from typing import Callable
 
 import torch
 import torch.nn as nn
-from torch.export import ExportedProgram, export, save
 
 from adaptive_echo_python.encoder import Encoder
 
 
-def export_graph(path: Path, target: Callable | nn.Module, args: tuple):
-    program: ExportedProgram = export(target, args)
-    save(program, path)
+def export_torchscript(path: Path, model: nn.Module, example_input: torch.Tensor):
+    traced_model = torch.jit.trace(model, example_input)
+    traced_model.save(str(path))
+
+    with torch.inference_mode():
+        output = traced_model(example_input)
+        print("output.shape:", output.shape)
 
 
 def export_graphs():
@@ -18,15 +20,22 @@ def export_graphs():
     encoder_embedding_size = 256
     encoder_hidden_size = 256
     encoder_num_layers = 6
-    export_graph(
-        Path("graphs/audio_encoder.pt2"),
-        Encoder(
-            audio_encoder_input_size,
-            encoder_embedding_size,
-            encoder_hidden_size,
-            encoder_num_layers,
-        ),
-        (torch.randn(1, audio_encoder_input_size),),
+
+    model = Encoder(
+        audio_encoder_input_size,
+        encoder_embedding_size,
+        encoder_hidden_size,
+        encoder_num_layers,
+    )
+
+    example_input_shape = (1, audio_encoder_input_size)
+    example_input = torch.randn(example_input_shape)
+
+    # Export as TorchScript format (.pt) for C++ compatibility
+    export_torchscript(
+        Path("graphs/audio_encoder.pt"),
+        model,
+        example_input,
     )
 
 
