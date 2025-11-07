0a30fb206bd5ad0692a6df325a8e3173d66f35a6
HenryTheall <thealh@rpi.edu>
2025-11-07 17:14:33 -0500

    interpolation.py

---
+++ adaptive_echo_python/src/adaptive_echo_python/interpolation.py
@@ -0,0 +1,9 @@
+import torch
+
+# t is a torch tensor in these function, a and b are floats, returns a torch tensor
+def linear_interp(a, b, t):
+    return a + (b - a) * t
+
+
+def exp_interp(a, b, t):
+    return a * (b / a).pow(t)
