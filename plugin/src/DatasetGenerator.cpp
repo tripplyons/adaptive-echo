e0efdc818a552b5ba7f5d68580961702f460d135
BoundlessFate <villac3@rpi.edu>
2025-11-21 16:48:16 -0500

    some error is causing dataset generator to crash. investigating, but very close to finishing

--- plugin/src/DatasetGenerator.cpp
+++ plugin/src/DatasetGenerator.cpp
@@ -2,7 +2,6 @@
 #include "WavHandler.hpp"
 #include <fstream>
 #include <iostream>
-#include <random>
 #include <stdexcept>
 #include <string>
 #include <thread>
@@ -20,36 +19,28 @@ void generateFile(const char *outputPath, torch::Tensor weights);
 // Runs on startup, self explanatory
 // Many checks to see if args are valid, and then calls rest of code
 int main(int argc, char **argv) {
-    try {
-        if (argc != 4) {
-            std::cout << "Usage: adaptive-echo <num-files> "
-                         "<local-path-to-output> <nthreads>"
-                      << std::endl;
-            return -1;
-        }
-        try {
-            int numFiles = std::stoi(argv[1]);
-            if (numFiles <= 0) {
-                throw std::invalid_argument("Argument less than or equal to 0");
-            }
-            int nThreads = std::stoi(argv[3]);
-            if (nThreads <= 0) {
-                throw std::invalid_argument("Argument less than or equal to 0");
-            }
-        } catch (const std::exception &e) {
-            std::cout << "Usage: argument 1,3 must be a positive integer"
-                      << std::endl;
-            return -1;
-        }
-        runProgram(std::stoi(argv[1]), argv[2], std::stoi(argv[3]));
-        return 0;
-        // A general catch all to avoid ugly popup on uncaught error. Blanket
-        // catch all
-    } catch (const std::exception &e) {
-        std::cout << "An unexpected error occured... exiting safely"
-                  << std::endl;
+    if (argc != 4) {
+        std::cout << "Usage: adaptive-echo <num-files> "
+                        "<local-path-to-output> <nthreads>"
+                    << std::endl;
         return -1;
     }
+    try {
+        int numFiles = std::stoi(argv[1]);
+        if (numFiles <= 0) {
+            throw std::invalid_argument("Argument less than or equal to 0");
+        }
+        int nThreads = std::stoi(argv[3]);
+        if (nThreads <= 0) {
+            throw std::invalid_argument("Argument less than or equal to 0");
+        }
+    } catch (const std::exception &e) {
+        std::cout << "Usage: argument 1,3 must be a positive integer"
+                    << std::endl;
+        return -1;
+    }
+    runProgram(std::stoi(argv[1]), argv[2], std::stoi(argv[3]));
+    return 0;
 }
 // Creates csv then creates files
 void runProgram(int numFiles, char *outputPath, int nThreads) {
@@ -81,32 +72,21 @@ vector<torch::Tensor> initializeCsv(char *outputPath, int numFiles) {
 // ------------------ FILE GENERATION BELOW --------------------------
 // Create individual file with given settings
 void generateFile(const char *outputPath, torch::Tensor weights) {
-    double maxTime = 5;
+    Synth synth("synth.pt");
+    synth.decodeSettings(weights);
+    float maxTime = 5;
     int sampleRate = 48000;
-    double timeInterval = 1.0 / sampleRate;
+    float timeInterval = 1.0 / sampleRate;
     int nSamples = maxTime * sampleRate;
-    vector<double> data;
-    data.reserve(nSamples);
-    // Add samples in order by time
-    std::mt19937 rng(std::random_device{}());
+    vector<float> times;
+    // Get vector of times
     for (int i = 0; i < nSamples; i++) {
-        double time = i * timeInterval;
-        double w0 = weights[0].item<double>();
-        double w1 = weights[1].item<double>();
-        double w2 = weights[2].item<double>();
-        double w3 = weights[3].item<double>();
-        double w4 = weights[4].item<double>();
-        double w5 = weights[5].item<double>();
-        double w6 = weights[6].item<double>();
-        double w7 = weights[7].item<double>();
-        double sample =
-            val(osc_uniform(rng, time, w0, w1, w2, w3, w4, w5, w6, w7));
-        double clipped = clamp(sample, -1.0, 1.0);
-        data.push_back(clipped);
+        times.push_back(i * timeInterval);
     }
+    vector<float> data = synth.generate(times);
     // Volume is also normalized alongside frequency
     // Find max absolute value for normalization
-    double maxAbs = 0.0;
+    float maxAbs = 0.0;
     for (int i = 0; i < nSamples; i++) {
         if (std::abs(data[i]) > maxAbs)
             maxAbs = std::abs(data[i]);
@@ -116,8 +96,7 @@ void generateFile(const char *outputPath, torch::Tensor weights) {
     vector<int32_t> normalizedData;
     normalizedData.reserve(nSamples);
     for (int i = 0; i < nSamples; i++) {
-        double normalized =
-            data[i] / maxAbs * 0.99; // scale to 99% of full amplitude
+        double normalized = data[i] / maxAbs * 0.99;
         int32_t sampleConverted = static_cast<int32_t>(
             normalized *
             static_cast<double>(std::numeric_limits<int32_t>::max()));
