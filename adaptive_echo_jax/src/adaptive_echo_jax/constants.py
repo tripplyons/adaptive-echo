bba6e7aa3b3e7cf4725347b97845737af9466e97
Tripp Lyons <tripplyons@gmail.com>
2026-01-31 16:02:11 -0500

    remove jax version

--- adaptive_echo_jax/src/adaptive_echo_jax/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Constants used for audio generation.
-"""
-
-num_seconds = 2
-training_sample_rate = 16384  # Sample rate for optimization (faster)
-output_sample_rate = 48000  # Sample rate for output files
-num_samples = training_sample_rate * num_seconds
